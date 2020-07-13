7a5454d6fdd593419ce8b658aec73e2ee29705c6
thatguywiththatname <simjenner3@gmail.com>
2020-07-13 01:35:27 +0100

    Fully features settings page now working

--- README.md
+++ README.md
@@ -19,8 +19,17 @@ When you type a query in the search bar, your browser makes a request to your br
 intercepts that request, and if the query matches a bang (e.g. `!m new york`) it will tell the browser to go the url set
 to that bang with the given query.
 
+## Options page
+
+Click on any cell to edit. The bang is what goes after the ! (e.g. !m) and the url is where it takes you.
+
+Use %s to show where the search query should be inserted (see the defaults for example).
+
+Cells with no text in will be highlighted red and will be removed upon save.
+
 ## Credit
 
 - [DuckDuckGo bangs](https://duckduckgo.com/bang)
 - [!Bang Quick Search](https://addons.mozilla.org/en-US/firefox/addon/bang-quick-search/)
 - [Water.css](https://github.com/kognise/water.css)
+- [Feather Icons](https://github.com/feathericons/feather)
--- options/options.js
+++ options/options.js
@@ -1,15 +1,30 @@
+// Highlights the save button to show something has changed and needs saving.
+function highlightSaveBtn() {
+    document.querySelector('#saveBtn').style.backgroundColor = 'darkblue';
+}
+
+// Opposite of highlightSaveBtn.
+function unHighlightSaveBtn() {
+    document.querySelector('#saveBtn').style.backgroundColor = '';
+}
+
+// Is called whenever a cell is edited, performs conditional highlights and save btn highlights.
 function onCellEdit(e) {
     if (e.target.textContent === '') {
         e.target.style.backgroundColor = '#FF6961';
     } else {
         e.target.style.backgroundColor = '';
     }
+    highlightSaveBtn();
 }
 
+// Event that fires when the delete button for a row is clicked.
 function deleteRowClicked(e) {
     e.target.parentElement.parentElement.remove();
+    highlightSaveBtn();
 }
 
+// Adds a row to the given table element with the given texts and a trash icon/
 function addRow(tableElem, bangText, urlText) {
     const row = document.createElement('tr');
 
@@ -28,15 +43,18 @@ function addRow(tableElem, bangText, urlText) {
     const trashImgTd = document.createElement('td');
     const trashImg = document.createElement('img');
     trashImg.src = 'trash.svg';
-    trashImg.className = 'trashBinSvg';
+    trashImg.className = 'trashSvg';
     trashImg.addEventListener('click', deleteRowClicked);
     trashImgTd.appendChild(trashImg);
     row.appendChild(trashImgTd);
 
     tableElem.appendChild(row);
+    highlightSaveBtn();
 }
 
-async function save(table) {
+// Takes the rows in the table and saves the given parameters to storage.
+async function saveFromTable() {
+    const table = document.querySelector('#bangsTable');
     const bangs = {};
 
     // Start from 1 to ignore headers.
@@ -54,22 +72,85 @@ async function save(table) {
     }
 
     await browser.storage.sync.set({ bangs });
+    unHighlightSaveBtn();
 }
 
-document.addEventListener('DOMContentLoaded', async () => {
+// Remove all <td> from the table and add new ones either the bangsToUse param or browser storage.
+async function renderTable(bangsToUse = null) {
     const table = document.querySelector('#bangsTable');
 
-    document.querySelector('#addRowBtn').addEventListener('click', async () => {
-        addRow(table, 'e', 'https://example.com?q=%s');
+    table.querySelectorAll('td').forEach((e) => {
+        // Remove every row (this will happen for each column in the row but that's fine).
+        e.parentElement.remove();
     });
 
-    document.querySelector('#saveBtn').addEventListener('click', async () => {
-        await save(table);
-    });
+    let bangs = bangsToUse;
+    if (bangs === null) {
+        bangs = (await browser.storage.sync.get('bangs')).bangs;
+    }
 
-    // It is assumed bangs != undefined as we set it in main.js.
-    const { bangs } = await browser.storage.sync.get('bangs');
     for (const [k, v] of Object.entries(bangs)) {
         addRow(table, k, v);
     }
+}
+
+// Imports bangs from a given file, saves them to the storage and then renders the table.
+// ToDo: Input validation!
+async function importBangs(fileInput) {
+    const file = fileInput.files[0];
+    const newBangs = JSON.parse(await file.text());
+    await renderTable(newBangs);
+}
+
+// Exports the currently saved bangs (not the bangs currently in the table).
+async function exportBangs() {
+    const { bangs } = await browser.storage.sync.get('bangs');
+    const dataStr = `data:text/json;charset=utf-8,${encodeURIComponent(JSON.stringify(bangs))}`;
+    const a = document.createElement('a');
+    a.setAttribute('href', dataStr);
+    a.setAttribute('download', 'custombangs.json');
+    a.click();
+    a.remove();
+}
+
+// Sets the storage to the defaults and re-renders the table.
+async function setDefaults() {
+    const r = await fetch('../defaults.json');
+    const bangs = await r.json();
+    await renderTable(bangs);
+}
+
+// Sets up event listeners and calls renderTable.
+document.addEventListener('DOMContentLoaded', async () => {
+    const table = document.querySelector('#bangsTable');
+    const fileInput = document.querySelector('#importFileInput');
+
+    document.querySelector('#saveBtn').addEventListener('click', async () => {
+        await saveFromTable();
+    });
+
+    document.querySelector('#addRowBtn').addEventListener('click', () => {
+        addRow(table, 'e', 'https://example.com?q=%s');
+    });
+
+    document.querySelector('#importBtn').addEventListener('click', () => {
+        fileInput.click();
+    });
+
+    fileInput.addEventListener('change', async () => {
+        await importBangs(fileInput);
+    });
+
+    document.querySelector('#exportBtn').addEventListener('click', async () => {
+        await exportBangs();
+    });
+
+    document.querySelector('#setDefaultBtn').addEventListener('click', async () => {
+        await setDefaults();
+    });
+
+    // It is assumed bangs wont ever be undefined here as we set it in main.js.
+    await renderTable();
+    // renderTable will cause the save button to highlight.
+    unHighlightSaveBtn();
 });
