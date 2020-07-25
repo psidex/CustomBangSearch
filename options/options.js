733229bce1414c8effe196a7e7174e07687743f0
thatguywiththatname <simjenner3@gmail.com>
2020-07-25 18:37:17 +0100

    Cleanup options.js

--- options/options.js
+++ options/options.js
@@ -1,30 +1,40 @@
-// Highlights the save button to show something has changed and needs saving.
-function highlightSaveBtn() {
-    document.querySelector('#saveBtn').style.backgroundColor = 'darkblue';
-}
+const saveBtnHighlightBgColour = 'darkblue';
+const emptyCellBgColour = '#FF6961';
+const defaultJsonFilePath = '../default.json';
+const exportFileName = 'custombangs.json';
+const helpUrl = 'https://github.com/psidex/CustomBangSearch#options-page';
 
-// Opposite of highlightSaveBtn.
-function unHighlightSaveBtn() {
-    document.querySelector('#saveBtn').style.backgroundColor = '';
+// Makes calls to setSaveBtnHighlight easier to read.
+const ON = true;
+const OFF = false;
+
+// Highlights the save button to show something has changed and needs saving.
+function setSaveBtnHighlight(onOff) {
+    if (onOff === ON) {
+        document.getElementById('saveBtn').style.backgroundColor = saveBtnHighlightBgColour;
+    } else {
+        document.getElementById('saveBtn').style.backgroundColor = '';
+    }
 }
 
 // Is called whenever a cell is edited, performs conditional highlights and save btn highlights.
 function onCellEdit(e) {
     if (e.target.textContent === '') {
-        e.target.style.backgroundColor = '#FF6961';
+        e.target.style.backgroundColor = emptyCellBgColour;
     } else {
         e.target.style.backgroundColor = '';
     }
-    highlightSaveBtn();
+    setSaveBtnHighlight(ON);
 }
 
-// Event that fires when the delete button for a row is clicked.
+// Event handler for the row delete button.
 function deleteRowClicked(e) {
+    // The target will be the img, the first parent a td and the second the row.
     e.target.parentElement.parentElement.remove();
-    highlightSaveBtn();
+    setSaveBtnHighlight(ON);
 }
 
-// Adds a row to the given table element with the given texts and a trash icon/
+// Adds a row to the given tableElem with the given texts and a trash icon.
 function addRow(tableElem, bangText, urlText) {
     const row = document.createElement('tr');
 
@@ -42,37 +52,14 @@ function addRow(tableElem, bangText, urlText) {
 
     const trashImgTd = document.createElement('td');
     const trashImg = document.createElement('img');
-    trashImg.src = 'trash.svg';
-    trashImg.className = 'trashSvg';
     trashImg.addEventListener('click', deleteRowClicked);
+    trashImg.setAttribute('src', 'trash.svg');
+    trashImg.setAttribute('class', 'trashSvg');
     trashImgTd.appendChild(trashImg);
     row.appendChild(trashImgTd);
 
     tableElem.appendChild(row);
-    highlightSaveBtn();
-}
-
-// Takes the rows in the table and saves the given parameters to storage.
-async function saveFromTable() {
-    const table = document.querySelector('#bangsTable');
-    const bangs = {};
-
-    // Start from 1 to ignore headers.
-    for (let i = 1; i < table.rows.length; i++) {
-        const row = table.rows[i];
-
-        const bangText = row.cells[0].textContent.trim();
-        const urlText = row.cells[1].textContent.trim();
-
-        if (bangText !== '' && urlText !== '') {
-            bangs[bangText] = urlText;
-        } else {
-            row.remove();
-        }
-    }
-
-    await browser.storage.sync.set({ bangs });
-    unHighlightSaveBtn();
+    setSaveBtnHighlight(ON);
 }
 
 // Remove all <td> from the table & add new ones from either bangsToUse or browser storage.
@@ -98,13 +85,44 @@ async function renderTable(bangsToUse = null) {
     }
 }
 
+// Takes the rows in the table and saves the given parameters to storage.
+async function saveFromTable() {
+    const table = document.querySelector('#bangsTable');
+    const bangs = {};
+
+    // Start from 1 to ignore headers.
+    for (let i = 1; i < table.rows.length; i++) {
+        const row = table.rows[i];
+
+        const bangText = row.cells[0].textContent.trim();
+        const urlText = row.cells[1].textContent.trim();
+
+        if (bangText !== '' && urlText !== '') {
+            bangs[bangText] = urlText;
+        } else {
+            row.remove();
+        }
+    }
+
+    await browser.storage.sync.set({ bangs });
+    // Will remove duplicate bangs.
+    await renderTable(bangs);
+    setSaveBtnHighlight(OFF);
+}
+
 // Attempts to load a valid & importable JSON object from the file, otherwise returns null.
 async function tryFileToObj(file) {
     if (file.type !== 'application/json') {
         return null;
     }
 
-    const obj = JSON.parse(await file.text());
+    let obj = {};
+    try {
+        obj = JSON.parse(await file.text());
+    } catch (e) {
+        return null;
+    }
+
     for (const [key, value] of Object.entries(obj)) {
         if (typeof key !== 'string' || typeof value !== 'string') {
             return null;
@@ -127,6 +145,7 @@ async function importBangs(fileInput) {
         // Show and then hide error toast.
         const toast = document.querySelector('#errorToast');
         toast.className = 'show';
+        // Timeout is linked to time in CSS.
         setTimeout(() => { toast.className = ''; }, 4000);
     }
 }
@@ -137,14 +156,14 @@ async function exportBangs() {
     const dataStr = `data:text/json;charset=utf-8,${encodeURIComponent(JSON.stringify(bangs))}`;
     const a = document.createElement('a');
     a.setAttribute('href', dataStr);
-    a.setAttribute('download', 'custombangs.json');
+    a.setAttribute('download', exportFileName);
     a.click();
     a.remove();
 }
 
 // Sets the storage to the defaults and re-renders the table.
 async function setDefaults() {
-    const r = await fetch('../defaults.json');
+    const r = await fetch(defaultJsonFilePath);
     const bangs = await r.json();
     await renderTable(bangs);
 }
@@ -179,12 +198,12 @@ document.addEventListener('DOMContentLoaded', async () => {
     });
 
     document.querySelector('#helpBtn').addEventListener('click', async () => {
-        window.open('https://github.com/psidex/CustomBangSearch#options-page');
+        window.open(helpUrl);
     });
 
     // It is assumed bangs wont ever be undefined here as we set it in main.js.
     await renderTable();
     // renderTable will cause the save button to highlight.
     // We're rendering what is already saved so don't do that.
-    unHighlightSaveBtn();
+    setSaveBtnHighlight(OFF);
 });
